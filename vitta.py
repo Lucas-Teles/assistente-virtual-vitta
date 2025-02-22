b2177e0d9a2db832cfd2c80befe7d4e3fef6d83c
Lucas Teles <lucasouzateles@gmail.com>
2025-02-22 18:15:44 -0300

    :tada: Commit inicial

---
+++ vitta.py
@@ -0,0 +1,7 @@
+
+
+print("/" + "-" * 60 + "\\")
+print("|{:^60}|".format(" Olá, eu sou a VITTA! "))
+print("|{:^60}|".format(" Sua assistente virtual de bem estar e saúde"))
+print("\\" + "-" * 60 + "/")
+
