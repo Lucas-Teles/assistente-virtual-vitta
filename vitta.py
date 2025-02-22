ce17e4d4c878b228df529d379c7dc9feb326cd44
Lucas Teles <lucasouzateles@gmail.com>
2025-02-22 18:55:52 -0300

    :sparkles: feat: Introduzindo menu e calculo imc

--- vitta.py
+++ vitta.py
@@ -1,7 +1,50 @@
+import time
+
+print("+" + "-" * 60 + "+")
+print("|{:^60}|".format(" Olá, boas-vindas! "))
+print("|{:^60}|".format(" Sou a Vitta "))
+print("|{:^60}|".format(" Sua assistente virtual de saúde e bem-estar. "))
+print("+" + "-" * 60 + "+")
+print("\nPara iniciar, vou precisar de algumas informações sobre você.\n")
+print("+" + "-" * 60 + "+")
 
 
-print("/" + "-" * 60 + "\\")
-print("|{:^60}|".format(" Olá, eu sou a VITTA! "))
-print("|{:^60}|".format(" Sua assistente virtual de bem estar e saúde"))
-print("\\" + "-" * 60 + "/")
+# Entrada de dados
+nome = input("Qual o seu nome? ")
+idade = int(input("Qual a sua idade? "))
+peso = float(input("Qual o seu peso? "))
+altura = float(input("Qual a sua altura? (ex: 1.70) "))
+print("+" + "-" * 60 + "+")
 
+
+print("\n+" + "-" * 60 + "+")
+print("|{:^60}|".format(f" Olá, {nome}! "))
+print("|{:^60}|".format(" Analisando seus dados... "))
+print("+" + "-" * 60 + "+")
+time.sleep(1.5)
+
+# Calculo do IMC ... 
+imc = peso / (altura ** 2)
+
+print("|{:<60}|".format(f" Seu IMC é: {imc:.2f} "))
+
+if imc < 18.5:
+    status = " No momento seu imc está informando -> abaixo do peso. "
+elif imc < 24.9:
+    status = " No momento seu imc está informando -> peso normal. "
+elif imc < 29.9:
+    status = " No momento seu imc está informando -> sobrepeso. "
+elif imc < 34.9:
+    status = " No momento seu imc está informando -> obesidade grau 1. "
+elif imc < 39.9:
+    status = " No momento seu imc está informando -> obesidade grau 2. "
+else:
+    status = " No momento seu imc está informando -> obesidade grau 3. "
+
+print("|{:<60}|".format(status))
+print("+" + "-" * 60 + "+")
+# Fim do calculo do IMC
+
+print("\n+" + "-" * 60 + "+")
+print("|{:<60}|".format(f" Seu IMC é: {imc:.2f} "))
+print("+" + "-" * 60 + "+")
